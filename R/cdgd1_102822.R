61886df05a2826d04c90c7b8d4b9dd46e45bcda3
Ang Yu <ang_yu@outlook.com>
2022-11-02 16:45:10 -0500

    Cont

--- R/cdgd1_102822.R
+++ R/cdgd1_102822.R
@@ -241,7 +241,7 @@ cdgd1 <- function(Y,D,G,X,Q,data,algorithm) {
   Y1givenQ.Pred_G0[sample1] <- stats::predict(Y1givenGQ.Model.sample1, newdata = pred_data[sample2,])
   Y1givenQ.Pred_G0[sample2] <- stats::predict(Y1givenGQ.Model.sample2, newdata = pred_data[sample1,])
 
-  ### The "IPO" (individual potential outcome) function
+### The "IPO" (individual potential outcome) function
   # For each d and g value, we have IE(d,g)=\frac{\one(D=d)}{\pi(d,X,g)}[Y-\mu(d,X,g)]+\mu(d,X,g)
   # We stablize the weight by dividing the sample average of estimated weights
 
@@ -262,25 +262,127 @@ cdgd1 <- function(Y,D,G,X,Q,data,algorithm) {
   #  prop_G1[sample1] <- mean(data[sample2,G])
   #  prop_G1[sample2] <- mean(data[sample1,G])
 
-  ### The one-step estimate of \xi_{dgg'g''}
+### Estimate E(D | Q,g')
+  data[,D] <- as.factor(data[,D])
+  levels(data[,D]) <- c("D0","D1")  # necessary for caret implementation of ranger
+
+  if (algorithm=="nnet") {
+    message <- utils::capture.output( DgivenGQ.Model.sample1 <- caret::train(stats::as.formula(paste(D, paste(G,Q,sep="+"), sep="~")), data=data[sample1,], method="nnet",
+                                                                              preProc=c("center","scale"), trControl=caret::trainControl(method="cv"), linout=FALSE,
+                                                                              tuneGrid=expand.grid(size=c(1,2),decay=c(0,0.1,0.2,0.3)) ))
+    message <- utils::capture.output( DgivenGQ.Model.sample2 <- caret::train(stats::as.formula(paste(D, paste(G,Q,sep="+"), sep="~")), data=data[sample2,], method="nnet",
+                                                                              preProc=c("center","scale"), trControl=caret::trainControl(method="cv"), linout=FALSE,
+                                                                              tuneGrid=expand.grid(size=c(1,2),decay=c(0,0.1,0.2,0.3)) ))
+  }
+  if (algorithm=="ranger") {
+    message <- utils::capture.output( DgivenGQ.Model.sample1 <- caret::train(stats::as.formula(paste(D, paste(G,Q,sep="+"), sep="~")), data=data[sample1,], method="ranger",
+                                                                              trControl=caret::trainControl(method="cv", classProbs=TRUE),
+                                                                              tuneGrid=expand.grid(mtry=c(5,10,15,20),splitrule=c("gini"),min.node.size=c(50,100,200))) )
+    message <- utils::capture.output( DgivenGQ.Model.sample2 <- caret::train(stats::as.formula(paste(D, paste(G,Q,sep="+"), sep="~")), data=data[sample2,], method="ranger",
+                                                                              trControl=caret::trainControl(method="cv", classProbs=TRUE),
+                                                                              tuneGrid=expand.grid(mtry=c(5,10,15,20),splitrule=c("gini"),min.node.size=c(50,100,200))) )
+  }
+  if (algorithm=="gbm") {
+    message <- utils::capture.output( DgivenGQ.Model.sample1 <- caret::train(stats::as.formula(paste(D, paste(G,Q,sep="+"), sep="~")), data=data[sample1,], method="gbm",
+                                                                              trControl=caret::trainControl(method="cv"),
+                                                                              tuneGrid=expand.grid(n.trees=c(25,75,125), interaction.depth=c(1,2,4), shrinkage=0.1, n.minobsinnode=c(5,10,15))) )
+    message <- utils::capture.output( DgivenGQ.Model.sample2 <- caret::train(stats::as.formula(paste(D, paste(G,Q,sep="+"), sep="~")), data=data[sample2,], method="gbm",
+                                                                              trControl=caret::trainControl(method="cv"),
+                                                                              tuneGrid=expand.grid(n.trees=c(25,75,125), interaction.depth=c(1,2,4), shrinkage=0.1, n.minobsinnode=c(5,10,15))) )
+  }
+
+  data[,D] <- as.numeric(data[,D])-1
+
+  DgivenQ.Pred_G0 <- DgivenQ.Pred_G1 <- rep(NA, nrow(data))
+
+  pred_data <- data
+  pred_data[,G] <- 0
+  DgivenQ.Pred_G0[sample1] <- stats::predict(DgivenGQ.Model.sample1, newdata = pred_data[sample2,], type="prob")[,2]
+  DgivenQ.Pred_G0[sample2] <- stats::predict(DgivenGQ.Model.sample2, newdata = pred_data[sample1,], type="prob")[,2]
+
+  pred_data <- data
+  pred_data[,G] <- 1
+  DgivenQ.Pred_G1[sample1] <- stats::predict(DgivenGQ.Model.sample1, newdata = pred_data[sample2,], type="prob")[,2]
+  DgivenQ.Pred_G1[sample2] <- stats::predict(DgivenGQ.Model.sample2, newdata = pred_data[sample1,], type="prob")[,2]
+
+### Estimate p_g(Q)=Pr(G=g | Q)
+  data[,G] <- as.factor(data[,G])
+  levels(data[,D]) <- c("G0","G1")  # necessary for caret implementation of ranger
+
+  if (algorithm=="nnet") {
+    message <- utils::capture.output( GgivenQ.Model.sample1 <- caret::train(stats::as.formula(paste(G, paste(Q,sep="+"), sep="~")), data=data[sample1,], method="nnet",
+                                                                             preProc=c("center","scale"), trControl=caret::trainControl(method="cv"), linout=FALSE,
+                                                                             tuneGrid=expand.grid(size=c(1,2),decay=c(0,0.1,0.2,0.3)) ))
+    message <- utils::capture.output( GgivenQ.Model.sample2 <- caret::train(stats::as.formula(paste(G, paste(Q,sep="+"), sep="~")), data=data[sample2,], method="nnet",
+                                                                             preProc=c("center","scale"), trControl=caret::trainControl(method="cv"), linout=FALSE,
+                                                                             tuneGrid=expand.grid(size=c(1,2),decay=c(0,0.1,0.2,0.3)) ))
+  }
+  if (algorithm=="ranger") {
+    message <- utils::capture.output( GgivenQ.Model.sample1 <- caret::train(stats::as.formula(paste(G, paste(Q,sep="+"), sep="~")), data=data[sample1,], method="ranger",
+                                                                             trControl=caret::trainControl(method="cv", classProbs=TRUE),
+                                                                             tuneGrid=expand.grid(mtry=c(5,10,15,20),splitrule=c("gini"),min.node.size=c(50,100,200))) )
+    message <- utils::capture.output( GgivenQ.Model.sample2 <- caret::train(stats::as.formula(paste(G, paste(Q,sep="+"), sep="~")), data=data[sample2,], method="ranger",
+                                                                             trControl=caret::trainControl(method="cv", classProbs=TRUE),
+                                                                             tuneGrid=expand.grid(mtry=c(5,10,15,20),splitrule=c("gini"),min.node.size=c(50,100,200))) )
+  }
+  if (algorithm=="gbm") {
+    message <- utils::capture.output( GgivenQ.Model.sample1 <- caret::train(stats::as.formula(paste(G, paste(Q,sep="+"), sep="~")), data=data[sample1,], method="gbm",
+                                                                             trControl=caret::trainControl(method="cv"),
+                                                                             tuneGrid=expand.grid(n.trees=c(25,75,125), interaction.depth=c(1,2,4), shrinkage=0.1, n.minobsinnode=c(5,10,15))) )
+    message <- utils::capture.output( GgivenQ.Model.sample2 <- caret::train(stats::as.formula(paste(G, paste(Q,sep="+"), sep="~")), data=data[sample2,], method="gbm",
+                                                                             trControl=caret::trainControl(method="cv"),
+                                                                             tuneGrid=expand.grid(n.trees=c(25,75,125), interaction.depth=c(1,2,4), shrinkage=0.1, n.minobsinnode=c(5,10,15))) )
+  }
+
+  data[,G] <- as.numeric(data[,G])-1
+
+  GgivenQ.Pred <- rep(NA, nrow(data))
+  GgivenQ.Pred[sample1] <- stats::predict(DgivenGQ.Model.sample1, newdata = data[sample2,], type="prob")[,2]
+  GgivenQ.Pred[sample2] <- stats::predict(DgivenGQ.Model.sample2, newdata = data[sample1,], type="prob")[,2]
+
+
+### The one-step estimate of \xi_{dgg'g''}
   # There are 8 possible dgg'g'' combinations, so we define a function first
   psi_dgg <- function(d,g1,g2,g3) {
     if (d==0 & g1==0) {
       IPO_arg <- IPO_D0G0
-      YgivenXQ.Pred_arg <- YgivenXQ.Pred_D0G0}
+      YdgivenQ.Pred_arg <- Y0givenQ.Pred_G0
+      g1givenQ.Pred_arg <- 1-GgivenQ.Pred}
     if (d==1 & g1==0) {
       IPO_arg <- IPO_D1G0
-      YgivenXQ.Pred_arg <- YgivenXQ.Pred_D1G0}
+      YdgivenQ.Pred_arg <- Y1givenQ.Pred_G0
+      g1givenQ.Pred_arg <- 1-GgivenQ.Pred}
     if (d==0 & g1==1) {
       IPO_arg <- IPO_D0G1
-      YgivenXQ.Pred_arg <- YgivenXQ.Pred_D0G1}
+      YdgivenQ.Pred_arg <- Y0givenQ.Pred_G1
+      g1givenQ.Pred_arg <- GgivenQ.Pred}
     if (d==1 & g1==1) {
       IPO_arg <- IPO_D1G1
-      YgivenXQ.Pred_arg <- YgivenXQ.Pred_D1G1}
+      YdgivenQ.Pred_arg <- Y1givenQ.Pred_G1
+      g1givenQ.Pred_arg <- GgivenQ.Pred}
 
-    psi_dgg_S1 <- mean( as.numeric(data[sample1,G]==g1)/mean(data[sample1,G]==g1)*IPO_arg[sample1]*mean(as.numeric(data[sample1,G]==g2)/mean(data[sample1,G]==g2)*data[sample1,D]) +
+    if (g2==0) {
+      DgivenQ.Pred_arg <- DgivenQ.Pred_G0
+      g2givenQ.Pred_arg <- 1-GgivenQ.Pred
+    }
+    if (g2==1) {
+      DgivenQ.Pred_arg <- DgivenQ.Pred_G1
+      g2givenQ.Pred_arg <- GgivenQ.Pred
+    }
+
+    if (g3==0) {
+      g3givenQ.Pred_arg <- 1-GgivenQ.Pred
+    }
+    if (g3==1) {
+      g3givenQ.Pred_arg <- GgivenQ.Pred
+    }
+
+    psi_dggg_S1 <- mean( as.numeric(data[sample1,G]==g3)/mean(data[sample1,G]==g3)*YdgivenQ.Pred_arg*DgivenQ.Pred_arg +
+                           as.numeric(data[sample1,G]==g1)/mean(data[sample1,G]==g3) )
+
+    psi_dggg_S1 <- mean( as.numeric(data[sample1,G]==g1)/mean(data[sample1,G]==g1)*IPO_arg[sample1]*mean(as.numeric(data[sample1,G]==g2)/mean(data[sample1,G]==g2)*data[sample1,D]) +
                           as.numeric(data[sample1,G]==g2)/mean(data[sample1,G]==g2)*mean(as.numeric(data[sample1,G]==g1)/mean(data[sample1,G]==g1)*YgivenXQ.Pred_arg)*(data[sample1,D]-mean(as.numeric(data[sample1,G]==g2)/mean(data[sample1,G]==g2)*data[sample1,D])) )
-    psi_dgg_S2 <- mean( as.numeric(data[sample2,G]==g1)/mean(data[sample2,G]==g1)*IPO_arg[sample2]*mean(as.numeric(data[sample2,G]==g2)/mean(data[sample2,G]==g2)*data[sample2,D]) +
+    psi_dggg_S2 <- mean( as.numeric(data[sample2,G]==g1)/mean(data[sample2,G]==g1)*IPO_arg[sample2]*mean(as.numeric(data[sample2,G]==g2)/mean(data[sample2,G]==g2)*data[sample2,D]) +
                           as.numeric(data[sample2,G]==g2)/mean(data[sample2,G]==g2)*mean(as.numeric(data[sample2,G]==g1)/mean(data[sample2,G]==g1)*YgivenXQ.Pred_arg)*(data[sample2,D]-mean(as.numeric(data[sample2,G]==g2)/mean(data[sample2,G]==g2)*data[sample2,D])) )
     # Note that this is basically DML1. We could also use DML2, i.e., we would directly return psi_dgg as follows
     # psi_dgg <- mean( as.numeric(data[,G]==g1)/mean(data[,G]==g1)*IPO_arg*mean(as.numeric(data[,G]==g2)/mean(data[,G]==g2)*data[,D]) +
